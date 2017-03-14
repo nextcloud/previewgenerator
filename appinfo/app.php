03be88f9d5d55f2fd48bf6d4d1589463aa137c2d
Roeland Jago Douma <roeland@famdouma.nl>
2017-03-14 22:03:16 +0100

    Add check for already running command
    At the start and after each processed file we now write a value to the
    appconfig. This value is cleared on a successfull exit. If another run
    is started before the current run is finished we check if the latest
    activity is >30 minutes ago. If so we assume the process is dead.
    
    Signed-off-by: Roeland Jago Douma <roeland@famdouma.nl>

--- appinfo/app.php
+++ appinfo/app.php
@@ -20,4 +20,4 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
-$app = new \OCA\PreviewGenerator\AppInfo\Application('PreviewGenerator');
+$app = new \OCA\PreviewGenerator\AppInfo\Application('previewgenerator');
