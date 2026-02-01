ed10d5b770cfc59242714f0a00464c07a6818178
Richard Steinmetz <richard@steinmetz.cloud>
2026-02-01 12:22:43 +0100

    chore: fix my license headers
    Change "Richard Steinmetz" to "Nextcloud GmbH and Nextcloud contributors".
    I am the sole author of the changed files so it is not an issue to
    change the headers in retrospect
    
    Signed-off-by: Richard Steinmetz <richard@steinmetz.cloud>

--- lib/Migration/Version051200Date20251109132659.php
+++ lib/Migration/Version051200Date20251109132659.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 /**
- * SPDX-FileCopyrightText: 2025 Richard Steinmetz
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
